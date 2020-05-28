998a8a1034d9c2a9a329cfbff86ddd821520bb84
Jason Pollentier <jason.pollentier@revelry.co>
2020-05-28 08:48:32 -0500

    Patch react-native-animated/mock for a couple of issues
    - https://github.com/react-navigation/react-navigation/issues/8151#issuecomment-630239090
    - https://github.com/software-mansion/react-native-reanimated/pull/622

--- jest.setup.js
+++ jest.setup.js
@@ -7,6 +7,15 @@ const mockKeyChain = {
   resetGenericPassword: jest.fn().mockResolvedValue(),
 }
 jest.mock('react-native-keychain', () => mockKeyChain)
-jest.mock('react-native-reanimated', () =>
-  require('react-native-reanimated/mock')
-)
+jest.mock('react-native-reanimated', () => {
+  const mock = require('react-native-reanimated/mock')
+  const getValue = (node) => {
+    if (typeof node === "number") {
+      return node
+    }
+    return node && node["__value"]
+  }
+  mock.default.cond = (a, b, c) => getValue(a) ? b : c
+  mock.default.call = (a, b) => {}
+  return mock
+})
