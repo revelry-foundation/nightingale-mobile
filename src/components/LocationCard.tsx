d7cd7afad09f55bda7dc6108b377e62c45d20251
Samuel Keer <samuelkeer@gmail.com>
2020-05-29 16:07:35 -0500

    fixed location card

--- src/components/LocationCard.tsx
+++ src/components/LocationCard.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {Text, TouchableHighlight, View} from 'react-native'
 import {Location} from '../containers/LocationStorageContainer'
+import {formatDate} from '../helpers/dates'
+import LoginStyles from '../styles/LogInStyles'
+
+const loginStyles = LoginStyles.createStyles()
 
 interface Props {
     location: Location
@@ -19,9 +23,11 @@ export default function LocationCard({
     <TouchableHighlight
       onPress={() => handleViewLocation(index, location)} // using index to delete or edit location
     >   
-    <Text>
-      {location.latitude}, {location.longitude} at {location.when}
-    </Text>
+
+<Text style={loginStyles.bodyCopy}>{location.address}</Text>
+<Text style={loginStyles.bodyCopySmall}>
+  {formatDate(location.when)}
+</Text>
     </TouchableHighlight>
   </View>
   )
