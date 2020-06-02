76025ad4d78fd8eeeac7d403388e3c50c18f02ba
Samuel Keer <samuelkeer@gmail.com>
2020-06-02 15:34:39 -0500

    added hotspot locations and styling to positivelocationsscreen

--- src/components/PositiveLocationsScreen.tsx
+++ src/components/PositiveLocationsScreen.tsx
@@ -9,6 +9,7 @@ import {formatDateTime} from '../helpers/dates'
 import * as Colors from '../styles/colors'
 import * as Spacing from '../styles/spacing'
 import * as Size from '../styles/sizes'
+import {baseCard, cardDivider} from '../styles/components/cards'
 
 const loginStyles = LoginStyles.createStyles()
 
@@ -27,21 +28,34 @@ class PositiveLocationsScreen extends Component<Props> {
             <View>
             <View style={loginStyles.spaceVertical}>
             <TouchableHighlight onPress={() => this.props.navigation.goBack()}>
-              <Text style={styles.buttonLink}>Back</Text>
+              <Text style={styles.buttonLink}>Back to Disclaimer</Text>
             </TouchableHighlight>
+            <Text style={{...loginStyles.h1, ...loginStyles.textCenter, ...styles.spaceVertical}}>
+          Covid 19 Hotspots
+        </Text>
+        <Text style={styles.spaceVertical}>Possible Matches Based on Date, Time and Location</Text>
+            <Text style={loginStyles.bodyCopySmall}>This list indicates possible matches 
+            between your locations and known hotspots. These possible matches are based on the date and time of each location.
+            </Text>
             </View>
               {this.props.isFetching && <Text>...loading locations...</Text>}
-              <View style={loginStyles.list}>
-                {this.props.positiveLocations.map(({location}) => (
-                  <View style={loginStyles.listItem} key={location.when}>
-                    <TouchableHighlight onPress={() => this.props.navigation.navigate('PositiveLocationScreen')}>
+              <View>
+                {this.props.positiveLocations.map(({location, positiveLocation}) => (
+                  <View style={[baseCard, styles.spaceVertical]} key={location.when}>
                     <View>
+                    <Text style={styles.spaceVertical}>My Location</Text>
                     <Text style={loginStyles.bodyCopy}>{location && location.address}</Text>
+                    <Text style={loginStyles.bodyCopySmall}>
+                    {location && formatDateTime(location.when)}
+            </Text> 
+        </View>
+                    <View style={cardDivider}>
+                    <Text style={styles.spaceVertical}>Hotspot</Text>
+                    <Text style={loginStyles.bodyCopy}>{JSON.stringify(positiveLocation)}</Text>
                     <Text style={loginStyles.bodyCopySmall}>
                     {location && formatDateTime(location.when)}
         </Text> 
       </View>
-    </TouchableHighlight>
                   </View>
                 ))}
               </View>
